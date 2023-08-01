0cf91ccba4c83ff5ec22311367c8721c5ccf5424
nobley3 <121958909+nobley3@users.noreply.github.com>
2023-08-01 21:58:50 +0900

    Add files via upload
    0801

---
+++ Library.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Library {
+
+	public void borrow() {
+		System.out.println("대출하셨습니다.");
+	}
+	public void RReturn() {
+		System.out.println("반납하셨습니다.");
+	}
+	public void overdue() {
+		System.out.println("연체되었습니다.");
+	}
+}
---
+++ LibraryTest.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class LibraryTest {
+
+	public static void main(String[] args) {
+	
+		자바의정석 J = new 자바의정석();
+		부의인문학 M = new 부의인문학();
+		
+		J.already_B();
+		M.already_R();
+	}
+
+}
